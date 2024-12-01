63a3ebee0edb1f5e682ec5e02843837551083df0
Mladen Batakovic <mladen.batakovic@webmine.it>
2024-12-01 18:28:34 +0100

    TG-14 Force https for production - allow admin to access panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+/**
+ * @property string name
+ * @property string email
+ * @property string password
+ * @property string remember_token
+ * @property string email_verified_at
+ */
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
